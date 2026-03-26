a5bc2d1abd80d23bac1ee8fc7259ae696ab843f5
txemi <txemitron@gmail.com>
2026-03-26 21:10:17 +0100

    Bump version to 0.80.1

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
-__version__ = "0.80.0"
-__git_commit__ = "5a9e18a7"
-__git_describe__ = "v0.80.0-0-g5a9e18a7"
+__version__ = "0.80.1"
+__git_commit__ = "d70f06b3"
+__git_describe__ = "v0.80.0-2-gd70f06b3-dirty"
