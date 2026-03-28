21efbd4cc7a46d0afd30f79baede953a56e0701e
txemi <txemitron@gmail.com>
2026-03-28 10:39:27 +0100

    Update version.py with tag v0.80.3 information

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
 __version__ = "0.80.3"
-__git_commit__ = "9bc05d3d"
-__git_describe__ = "jenkins-abort-19-504a6d7afe1a8a542521ad96382e510ae2b1edb9-1-g9bc05d3d-dirty"
+__git_commit__ = "46f5c172"
+__git_describe__ = "v0.80.3-0-g46f5c172-dirty"
